50fa2da3d41e146b28dd508c417e17f3c56f7b11
Roberta Conrad <rjcnrd@gmail.com>
2018-11-28 17:06:18 +0100

    added first version hover plot on shiny

--- shiny_front_roberta.R
+++ shiny_front_roberta.R
@@ -10,64 +10,57 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      selectInput("dropdown_name",label="Select the name", selected="Vincent",choices=NULL),
-      
-      selectInput("color",label="Color the graph", selected="pink",choices=c("pink","red","yellow","grey") ),
-      sliderInput("range", "Range:",
-                  min = 1900, max = 2017,
-                  value = c(1900,2017))
-      
-    ),
+      #selectInput("color",label="Color the graph", selected="pink",choices=c("pink","red","yellow","grey") ),
+      '   sliderInput("range", "Range:",
+      min = 1900, max = 2017,
+      value = c(1900,2017))'
+  ),
+  
+  mainPanel(
     
-    mainPanel(
-      textOutput("text"),
-      
-      plotOutput("plot_popularity"),
-      
-      
-      # plotOutput("plot_popularity"),
-      dataTableOutput("data")
-    )
+    plotlyOutput("plot"),
+    verbatimTextOutput("event")
+    
   )
-)
+  )
+  )
 
 server <- function(input, output,session) {
   
-  library(prenoms)
   library(glue)
   library(tidyverse)
+  library(tidyverse)
+  library(plotly)
+  library(billboard)
+  library(prenoms)
   
-  data(prenoms)
+  '  plot_cross <- function(database,year="1960",x_axis=energy,y_axis=danceability){
+  year <- enquo(year)
+  x_axis <- enquo(x_axis)
+  y_axis <- enquo(y_axis)
+  x_axis_name <- as.character(x_axis)
+  y_axis_name <- as.character(y_axis)
+  tracklist <- database %>% filter(year == {!!year} | year == "0" ) %>% select(year,artist_name,track_name,!!x_axis,!!y_axis)
   
-  draw_a_name <- function(nom,color,beginning) {
-    prenoms %>%
-      filter(name == nom) %>%
-      group_by(year, name) %>%
-      summarise(total = sum(n)) %>%
-      ungroup() %>%
-      complete(year = 1900:2017, name, fill = list(total = 0)) %>%
-      # replace_na(list(total = 0)) %>%
-      ggplot() +
-      aes(x = year, y = total) +
-      geom_line(color=color) + xlim(beginning)
-  }
+  #if we have a "new" element, we show this in a different color, otherwise we will simply display all points in grey
   
-  count_a_name <- function(nom){
-    prenoms %>% 
-      filter(name == nom) %>% 
-      summarise(total = sum(n)) %>% 
-      pull(total)
-  }
+  plot <- ggplot(tracklist, aes(!!x_axis, !!y_axis))  +
+  geom_point(aes(text = track_name, artist= artist_name, size = 0.1),alpha = 1/2) + theme_minimal() + 
+  xlim(0,1) + ylim (0,1)
   
-  get_list<- function(){
-    prenoms %>% 
-      filter(name == nom) %>% 
-      summarise(total = sum(n)) %>% 
-      pull(total)
-  }
+  #  ggplotly(plot, tooltip = c("text", "artist",glue::glue("{x_axis_name}"),glue::glue("{y_axis_name}") ))
   
+}'
   
+  tracklist <- spotify_track_data %>% filter(year == "2015" | year == "0" ) %>% select(year,artist_name,track_name,danceability, energy)
   
+  plot_test <- ggplot(tracklist, aes(danceability, energy))  +
+    geom_point(aes(text = track_name, artist= artist_name, size = 0.1),alpha = 1/2) + theme_minimal() + 
+    xlim(0,1) + ylim (0,1)
+  
+  plotly_test <-  ggplotly(plot_test, tooltip = c("text", "artist","danceability", "energy"))
+
+
   output$data  <- reactive({renderDataTable(prenoms %>% filter(name == input$name) )
   })
   
@@ -80,15 +73,17 @@ server <- function(input, output,session) {
                       choices=test)
   })
   
-  output$text <- renderText({  glue('There are {count_a_name(input$dropdown_name)} {input$dropdown_name}.') 
+  
+  output$plot <- renderPlotly({
+    plotly_test 
   })
   
-  output$plot_popularity <- renderPlot({
-    draw_a_name(input$dropdown_name, input$color,input$range)
+  output$event <- renderPrint({
+    d <- event_data("plotly_hover")
+    if (is.null(d)) "Hover on a point!" else d
   })
   
-  
-}
+  }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
