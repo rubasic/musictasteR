c2ac98fd3ea07e25fb80e178bec43b6f3a61b685
Jorgen Lund <jorglund@outlook.com>
2018-12-06 16:13:46 +0100

    Updating get_tracks function and updating Shiny search.

--- working_files/jorgen-search/get_tracks.R
+++ working_files/jorgen-search/get_tracks.R
@@ -16,47 +16,48 @@
 #' }
 
 get_tracks <- function(track_name, artist_name = NULL, album_name = NULL, return_closest_track = FALSE, access_token = get_spotify_access_token()) {
+  
+  string_search <- track_name
+  
+  if (!is.null(artist_name)) {
+    string_search <- paste(string_search, artist_name)
+  }
+  
+  if (!is.null(album_name)) {
+    string_search <- paste(string_search, album_name)
+  }
+  
+  # Search Spotify API for track name
+  res <- GET('https://api.spotify.com/v1/search',
+             query = list(q = string_search,
+                          type = 'track',
+                          access_token = access_token)
+  ) %>% content
+  
+  if (length(res$tracks$items) >= 0) {
     
-    string_search <- track_name
+    res <- res %>% .$tracks %>% .$items
     
-    if (!is.null(artist_name)) {
-        string_search <- paste(string_search, artist_name)
+    tracks <- map_df(seq_len(length(res)), function(x) {
+      list(
+        track_name = res[[x]]$name,
+        track_uri = gsub('spotify:track:', '', res[[x]]$uri),
+        artist_name = res[[x]]$artists[[1]]$name,
+        artist_uri = res[[x]]$artists[[1]]$id,
+        album_name = res[[x]]$album$name,
+        album_id = res[[x]]$album$id,
+        album_img = res[[x]]$album$images[[1]]$url, # Album image URL
+        track_artist = paste(res[[x]]$name, res[[x]]$artists[[1]]$name, sep = " - ") # Track and artist name, separated by " - "
+      )
+    })
+    
+    if (return_closest_track == TRUE) {
+      tracks <- slice(tracks, 1)
     }
     
-    if (!is.null(album_name)) {
-        string_search <- paste(string_search, album_name)
-    }
-    
-    # Search Spotify API for track name
-    res <- GET('https://api.spotify.com/v1/search',
-               query = list(q = string_search,
-                            type = 'track',
-                            access_token = access_token)
-    ) %>% content
-    
-    if (length(res$tracks$items) >= 0) {
-        
-        res <- res %>% .$tracks %>% .$items
-        
-        tracks <- map_df(seq_len(length(res)), function(x) {
-            list(
-                track_name = res[[x]]$name,
-                track_uri = gsub('spotify:track:', '', res[[x]]$uri),
-                artist_name = res[[x]]$artists[[1]]$name,
-                artist_uri = res[[x]]$artists[[1]]$id,
-                album_name = res[[x]]$album$name,
-                album_id = res[[x]]$album$id,
-                album_img = res[[x]]$album$images[[1]]$url # Jorgen: added album_img 
-            )
-        })
-        
-        if (return_closest_track == TRUE) {
-            tracks <- slice(tracks, 1)
-        }
-        
-    } else {
-        tracks <- tibble()
-    }
-    
-    return(tracks)
+  } else {
+    tracks <- tibble()
+  }
+  
+  return(tracks)
 }
--- working_files/jorgen-search/shiny-jorgen-search.R
+++ working_files/jorgen-search/shiny-jorgen-search.R
@@ -12,57 +12,58 @@ ui <- shinyUI(fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      textInput("track", "Name a track"),
+      
+      ## ELEMENTS RELATED TO TRACK SEARCH
+      textInput("track", "Search for a track and/or an artist"),
       htmlOutput("albumImage"),
-      # selectInput("selectTrack", label = "3. Select a track", choices = NULL),
+      # selectInput("selectTrack", label = "3. Select a track", choices = NULL),    
+      checkboxGroupInput("selectTracks", label = "Select tracks", choices = NULL),
+      actionButton("addTracks", label = "Add tracks"),
+      tableOutput("yourTracks"),
+      textOutput("count"),
+      textOutput("class"),
+      textOutput("length"),
+      actionButton("clearTracks", label = "Clear tracks")
+      
+      ## ELEMENTS RELATED TO ARTIST SEARCH
       # textInput("artist", "1. Name an artist:"),
       # htmlOutput("artistImage"),
       # selectInput("selectArtist", label = "2. Choose an artist", choices = NULL),
-      checkboxGroupInput("selectTracks", label = "Select tracks", choices = NULL),
-      actionButton("addTracks", label = "Add tracks")
+      
     ),
     
     mainPanel(
-      tableOutput("artistTable"),
-      tableOutput("trackTable")
+      # tableOutput("artistTable"),
+      tableOutput("trackTable"),
+      # tableOutput("savedTable")
+      tableOutput("tempTable")
     )
   )
 ))
 
 server <- shinyServer(function(input, output, session) {
   
-  # Get access token 
+  # Get Spotify access token 
   Sys.setenv(SPOTIFY_CLIENT_ID = 'a98864ad510b4af6851331638eec170f')
   Sys.setenv(SPOTIFY_CLIENT_SECRET = '6445326414dd4bf381afbc779b182223')
   access_token <- get_spotify_access_token()
   
-  # Pulling list of artists from Spotify 
-  artists <- reactive({
-    req(input$artist)
-    get_artists(artist_name = input$artist, access_token = access_token) 
-  })
-  
+  ## SEARCH BY TRACK/ARTIST
   # Pulluing list of tracks from Spotify
   tracks <- reactive({
     req(input$track)
-    if(!is.null(input$artist)) {
-      get_tracks(track_name = input$track, artist_name = input$artist, access_token = access_token)
-    }            
+    # if(!is.null(input$artist)) {
+    #     get_tracks(track_name = input$track, artist_name = input$artist, access_token = access_token)
+    # }            
     get_tracks(track_name = input$track, access_token = access_token)
   })
   
-  # Showing the list of potential matches 
-  output$artistTable <- renderTable({
-    artists()$artist_name
-  },
-  colnames = FALSE)
-  
-  #Showing the list of potential track matches
+  # Showing the list of track matches
   output$trackTable <- renderTable({
-    tracks() %>% select(track_name, artist_name)
+    tracks() %>% select(track_artist)
   })
   
-  # Showing image of the first potential Track match
+  # Showing image of the first track match
   output$albumImage <- renderUI({
     image_url <- tracks()$album_img[1]
     tags$img(src = image_url, height = 200, width = 200)
@@ -70,15 +71,61 @@ server <- shinyServer(function(input, output, session) {
   
   # Updating the checkboxes that lets the user choose tracks 
   observeEvent(input$track, {
-    choices <- tracks()$track_name
-    artist <- tracks()$artist_name
-    updateCheckboxGroupInput(session = session, inputId = "selectTracks", choices = choices[1:5])
+    choices <- paste(tracks()$track_name, " - ", tracks()$artist_name)
+    updateCheckboxGroupInput(
+      session = session, inputId = "selectTracks", 
+      choices = choices[1:5])
   })
   
-  # Showing image of the first potential Artist match
-  # output$artistImage <- renderUI({
-  #   image_url <- artists()$artist_img[1]
-  #   tags$img(src = image_url, height = 200, width = 200)
+  selected <- data_frame()
+  
+  # Saving the selected tracks to a dataframe 
+  temp <- c()
+  observeEvent(input$addTracks, {
+    for(i in 1:length(input$selectTracks))
+      temp <<- rbind(temp, input$selectTracks[i])
+    output$tempTable <- renderTable({
+      temp
+    })
+  })
+  
+  # Adding chosen tracks to the master dataframe 
+  master_df <- data_frame()
+  
+  
+  # Clearing the dataframe with saved tracks
+  observeEvent(input$clearTracks, {
+    temp <<- c()
+    output$tempTable <- renderTable({
+      temp
+    })
+    
+  })
+  
+  # Output used for debugging and testing
+  observeEvent(input$selectTracks, {
+    selected <<- input$selectTracks
+    
+    output$count <- renderText({
+      selected
+    })
+    
+    output$class <- renderText({
+      selected %>% class()
+    })
+    
+    output$length <- renderText({
+      length(input$selectTracks)
+    })
+  })
+  
+  # From Sentify
+  # artist_audio_features <- eventReactive(input$tracks_go, {
+  #     df <- get_artist_audio_features(artist_info()$artist_uri[artist_info()$artist_name == input$select_artist], access_token = spotify_access_token(), parallelize = TRUE)
+  #     if (nrow(df) == 0) {
+  #         stop("Sorry, couldn't find any tracks for that artist's albums on Spotify.")
+  #     }
+  #     return(df)
   # })
   
   # Updating dropdown manu with potential matches on Track
@@ -87,11 +134,33 @@ server <- shinyServer(function(input, output, session) {
   #   updateSelectInput(session, "selectTrack", choices = choices)
   # })
   
-  # Updating dropdown manu with potential matches on Artist
+  
+  ## SEARCH BY ARTIST
+  # Pulling list of artists from Spotify 
+  # artists <- reactive({
+  #     req(input$artist)
+  #     get_artists(artist_name = input$artist, access_token = access_token) 
+  # })
+  
+  # # Showing the list of potential artist matches 
+  # output$artistTable <- renderTable({
+  #     artists()$artist_name
+  #     
+  # },
+  # colnames = FALSE)
+  
+  # Showing image of the first artist match
+  # output$artistImage <- renderUI({
+  #   image_url <- artists()$artist_img[1]
+  #   tags$img(src = image_url, height = 200, width = 200)
+  # })
+  
+  # Updating dropdown menu with artist matches
   # observeEvent(input$artist, {
   #   choices <- artists()$artist_name
   #   updateSelectInput(session, "selectArtist", choices = choices)
   # })
+  
 })
 
 # Run the application 
