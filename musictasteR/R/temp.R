0d13554e4a6c6e49b931a26400f429cd51c1e658
Kumbidou <claradionet@gmail.com>
2018-12-07 23:02:01 +0100

    average songs data file added

--- musictasteR/R/temp.R
+++ musictasteR/R/temp.R
@@ -1,8 +1,8 @@
 library(readr)
 library(dplyr)
 
-
 nonchart_df <- read_csv("raw_data/150k_sample.csv")
 nonchart_df <- nonchart_df %>% select(-c(1:2))
 colnames(nonchart_df)[5] <- "year"
 
+save(nonchart_df,file="nonchart_df.RData")
