7dbbe35b87d352883b5ebb7ff0a381d693217611
Roberta Conrad <rjcnrd@gmail.com>
2018-12-10 10:14:41 +0100

    added function get_track_artists and access it from

--- musictasteR/inst/my_app/server.R
+++ musictasteR/inst/my_app/server.R
@@ -1,5 +1,5 @@
 
-
+library(musictasteR)
 library(plotly)
 library(shiny)
 library(ggplot2)
@@ -61,42 +61,6 @@ hover.plot.shiny <- function(data,x,y,chosen_year)
   #                                                                                                                                                                                       color = "black")));
   return(hover.plot)
 }
-
-
-get_tracks_artists <- function(track_artist_name, access_token = get_spotify_access_token()) {
-
-  # Search Spotify API
-  res <- GET('https://api.spotify.com/v1/search',
-             query = list(q = track_artist_name,
-                          type = 'track,artist',
-                          access_token = access_token)
-  ) %>% content
-
-  if (length(res$tracks$items) >= 0) {
-
-    res <- res %>% .$tracks %>% .$items
-
-    tracks <- map_df(seq_len(length(res)), function(x) {
-      list(
-        track_artist_name = res[[x]]$name,
-        track_uri = gsub('spotify:track:', '', res[[x]]$uri),
-        artist_name = res[[x]]$artists[[1]]$name,
-        artist_uri = res[[x]]$artists[[1]]$id,
-        album_name = res[[x]]$album$name,
-        album_id = res[[x]]$album$id,
-        album_img = res[[x]]$album$images[[1]]$url,
-        release_date = res[[x]]$album$release_date,
-        track_artist = paste(res[[x]]$name, res[[x]]$artists[[1]]$name, sep = " - ") # Track and artist name combined
-      )
-    })
-
-  } else {
-    tracks <- tibble()
-  }
-
-  return(tracks)
-}
-
 
 
 shinyServer(function(input, output,session) {
