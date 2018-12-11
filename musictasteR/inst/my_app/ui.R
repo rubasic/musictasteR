f1d0e633a9ebea29a71120355877ca080e0fe3bf
Jorgen Lund <jorglund@outlook.com>
2018-12-11 22:35:55 +0100

    Updating Shiny header.

--- musictasteR/inst/my_app/ui.R
+++ musictasteR/inst/my_app/ui.R
@@ -14,7 +14,7 @@ fluidPage(theme = shinytheme("slate"),
   headerPanel("",
             tags$head(
               tags$img(src="https://raw.githubusercontent.com/rubasic/rubasic/master/musictasteR/inst/my_app/www/headrrrr.png",
-                       height = 50, style = "display: block; margin-left: 40px;
+                       height = 60, style = "display: block; margin-left: 40px;
                        margin-top:25px; margin-bottom: 0px;")
             )),
 
