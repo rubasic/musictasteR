620f8e20948f37fe3a7f66325916128730c57613
Roberta Conrad <rjcnrd@gmail.com>
2018-12-12 23:10:03 +0100

    added 2 new data objects & built

---
+++ musictasteR/R/billboard_music_dataframe.R
@@ -0,0 +1,7 @@
+#' Billboard Music Dataframe
+#'
+#' A dataset containing the top billboard charts from 1960-2018
+#'
+#' @format A data frame with 5497 rows and 23 variables. Each song has several attributes (from Spotify API)
+
+"billboard_music_dataframe"
